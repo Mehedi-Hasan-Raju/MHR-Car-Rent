1d11329b4725aa1742c6b4fcd5e39380ff94a04e
Mehedi-Hasan-Raju <tecmehedi7@gmail.com>
2026-07-30 20:50:19 +0600

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Listings from "./pages/Listings.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/listings" element={<Listings />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
